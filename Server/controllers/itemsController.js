f29779a8f0bfc34bd9a5cba38a7fdcc37f8afc84
iliasen <113366047+iliasen@users.noreply.github.com>
2023-03-30 20:51:23 +0300

    Crate POST&GET metods to include and get data in DB

--- Server/controllers/itemsController.js
+++ Server/controllers/itemsController.js
@@ -1,5 +1,24 @@
+const uuid = require('uuid')
+const path = require('path')
+const ApiError = require('../error/ApiError')
+
+const {Item} = require('../models/models')
 class ItemsController{
-    async create(req,res){}
+    async create(req,res, next){
+        try{
+            const {name,price, brandID,typeID,info}=req.body
+            const {img} = req.files
+            let fileName = uuid.v4() + ".jpg"
+            img.mv(path.resolve(__dirname,'..','static',fileName))//dirname - путь до текущей папрки, ('..') - вернутся на дерикторию назад, 3 - й параметр папка назначения в нашем случае static
+
+            const item =await Item.create({name,price,brandID,typeID,img:fileName})
+
+            return res.json(item)
+        }catch (e) {
+            next(ApiError.badRequest(e.massage))
+        }
+
+    }
     async getAll(req,res){}
     async getOne(req,res){}//получение данных о товаре по id
 }
--- Server/routes/itemsRouter.js
+++ Server/routes/itemsRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
+const itemsController = require('../controllers/itemsController')
 
-router.post('/',)
-router.get('/',)
-router.get('/:id',)
+router.post('/', itemsController.create)
+router.get('/', itemsController.getAll)
+router.get('/:id', itemsController.getOne)
 
 module.exports = router
