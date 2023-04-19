a0592bd9346ba7fc812f89d1b118e52f9f6fc865
iliasen <113366047+iliasen@users.noreply.github.com>
2023-04-20 00:02:57 +0300

    Creat JWT token tracker, also create roles and registration

--- Server/routes/itemRouter.js
+++ Server/routes/itemRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const itemsController = require('../controllers/itemController')
+const checkRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', itemsController.create)
+router.post('/',checkRole('ADMIN'), itemsController.create)
 router.get('/', itemsController.getAll)
 router.get('/:id', itemsController.getOne)
 
--- Server/routes/userRouter.js
+++ Server/routes/userRouter.js
@@ -1,10 +1,11 @@
 const Router = require('express')
 const router = new Router()
 const UserController = require('../controllers/userController')
+const authMiddleware = require('../middleware/authMiddleware')//проверка пользователя на авторизованность
 
-router.post('/registation', UserController.registation)
+router.post('/registration', UserController.registration)
 router.post('/login', UserController.login)
-router.get('/auth', UserController.check)
+router.get('/auth',authMiddleware, UserController.check)
 // router.delete('/',)// можно добавить
 
 module.exports = router
